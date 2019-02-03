9015914d1b13d5b4ead4954e61f40130f200a8be
Dzhevadov Eugene <e.dzhevadov@gmail.com>
2019-02-03 20:23:37 +0200

    feat(app): add store provider

--- source/index.js
+++ source/index.js
@@ -2,15 +2,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // Instruments
+import { store } from './init/store';
 import './theme/init';
 
 // Intro
 import App from './navigation/App';
 
 render(
-    <Router>
-        <App />
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
     , document.getElementById('app'));
