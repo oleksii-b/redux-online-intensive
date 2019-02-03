5d35706fbb27fd919d2abb30046294bdca512b06
Dzhevadov Eugene <e.dzhevadov@gmail.com>
2019-02-03 19:58:26 +0200

    feat(posts): add action types

--- source/index.js
+++ source/index.js
@@ -1,11 +1,16 @@
 // Core
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // Instruments
 import './theme/init';
 
 // Intro
-import Gallery from './basic-redux';
+import App from './navigation/App';
 
-render(<Gallery />, document.getElementById('app'));
+render(
+    <Router>
+        <App />
+    </Router>
+    , document.getElementById('app'));
