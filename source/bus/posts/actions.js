1421277829dac2b84db3a44ef06f7b9ec90b8768
Dzhevadov Eugene <e.dzhevadov@gmail.com>
2019-02-03 21:40:31 +0200

    feat(posts): create a new post

--- source/bus/posts/actions.js
+++ source/bus/posts/actions.js
@@ -1,5 +1,10 @@
 // Types
-import { FETCH_POST_ASYNC, FILL_POSTS } from './types';
+import {
+    FETCH_POST_ASYNC,
+    CREATE_POST_ASYNC,
+    FILL_POSTS,
+    ADD_NEW_POST
+} from './types';
 
 // Instruments
 import { api } from '../../REST';
@@ -8,6 +13,13 @@ export const fillPosts = (posts) => {
     return {
         type:    FILL_POSTS,
         payload: posts,
+    };
+};
+
+export const addNewPost = (post) => {
+    return {
+        type:    ADD_NEW_POST,
+        payload: post,
     };
 };
 
@@ -20,4 +32,15 @@ export const fetchPostsAsync = () => async (dispatch) => {
     const result = await response.json();
 
     dispatch(fillPosts(result.data));
+};
+
+export const createPostAsync = (comment) => async (dispatch) => {
+    dispatch({
+        type: CREATE_POST_ASYNC,
+    });
+
+    const response = await api.posts.create(comment);
+    const result = await response.json();
+
+    dispatch(addNewPost(result.data));
 };
