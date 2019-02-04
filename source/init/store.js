f3312cd36a6572ffb8d3c34e92012a9ffd8b78a3
Dzhevadov Eugene <e.dzhevadov@gmail.com>
2019-02-04 18:42:48 +0200

    feat(saga): add root-saga & saga-middleware

--- source/init/store.js
+++ source/init/store.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import { rootReducer } from './rootReducer';
+import { rootSaga } from './rootSaga';
 
 const logger = createLogger({ collapsed: true });
+const sagaMiddleware = createSagaMiddleware();
 const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const composeEnhancers = __DEV__ && devtools ? devtools : compose;
-const middleware = [thunk];
+const middleware = [sagaMiddleware, thunk];
 
 if (__DEV__) {
     middleware.push(logger);
@@ -16,3 +19,5 @@ if (__DEV__) {
 const enhancedStore = composeEnhancers(applyMiddleware(...middleware));
 
 export const store = createStore(rootReducer, enhancedStore);
+
+sagaMiddleware.run(rootSaga);
