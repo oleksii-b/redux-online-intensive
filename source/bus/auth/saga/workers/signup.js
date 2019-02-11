d944274bbf12cc35849cc875529f48e91d784988
Dzhevadov Eugene <e.dzhevadov@gmail.com>
2019-02-11 19:58:05 +0200

    feat(api): add signup

--- source/bus/auth/saga/workers/signup.js
+++ source/bus/auth/saga/workers/signup.js
@@ -1,5 +1,4 @@
 import { put, apply } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
 
 import { api } from '../../../../REST';
 import { authenticate } from '../../../auth/actions';
@@ -9,14 +8,13 @@ export function* signup ({ payload: userInfo }) {
     try {
         yield put(startFetching());
 
-        yield delay(2000);
-        // const response = yield apply(api, api.posts.fetch);
-        // const result = yield apply(response, response.json);
+        const response = yield apply(api, api.posts.signup, [userInfo]);
+        const result = yield apply(response, response.json);
 
-        // if (response.status !== 200) {
-        //     throw new Error(result.message);
-        // }
-        console.log('userInfo', userInfo);
+        if (response.status !== 200) {
+            throw new Error(result.message);
+        }
+
         yield put(authenticate());
     } catch (error) {
         yield put(emitError(error, 'signup worker'));
